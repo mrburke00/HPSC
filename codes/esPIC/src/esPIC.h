803cfbff588a01909c91c048fc3b8fd9fc7addf8
Scott Runnels <srunnels@srconsult.com>
2021-09-16 21:05:40 -0600

    Initial commit for Lab 04

---
+++ codes/esPIC/src/esPIC.h
@@ -0,0 +1,76 @@
+//  ================================================================================
+//  ||                                                                            ||
+//  ||              esPIC                                                         ||
+//  ||              ------------------------------------------------------        ||
+//  ||              E L E C T R O S T I C   P A R T I C L E - I N - C E L L       ||
+//  ||                                                                            ||
+//  ||              D E M O N S T R A T I O N   C O D E                           ||
+//  ||              ------------------------------------------------------        ||
+//  ||                                                                            ||
+//  ||       Developed by: Scott R. Runnels, Ph.D.                                ||
+//  ||                     University of Colorado Boulder                         ||
+//  ||                                                                            ||
+//  ||                For: CU Boulder CSCI 4576/5576 and associated labs          ||
+//  ||                                                                            ||
+//  ||           Copyright 2020 Scott Runnels                                     ||
+//  ||                                                                            ||
+//  ||                     Not for distribution or use outside of the             ||
+//  ||                     this course.                                           ||
+//  ||                                                                            ||
+//  ================================================================================
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <omp.h>
+#include <chrono>
+#include <ctime>
+#include "stdio.h"
+#include "math.h"
+#include "string.h"
+
+using std :: string;
+using std :: vector;
+using std :: stringstream;
+using std :: cout;
+using std :: endl;
+
+#define rLOOP  for ( int r = 1 ; r <= nField    ; ++r )
+#define cLOOP  for ( int c = 1 ; c <= bandwidth ; ++c )
+#define iLOOP  for ( int i = 1 ; i <= nRealx    ; ++i )
+#define jLOOP  for ( int j = 1 ; j <= nRealy    ; ++j )
+#define sLOOP  for ( int s = 0 ; s <= nRealx+1  ; ++s )
+#define tLOOP  for ( int t = 0 ; t <= nRealy+1  ; ++t )
+
+
+typedef  vector<double>          VD;
+typedef  vector<vector<double> > VDD;
+typedef  vector<int>             VI;
+typedef  vector<vector<int> >    VII;
+
+
+void Exit()
+{
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Finalize();
+  exit(0);
+}
+
+
+void FatalError(string msg)
+{
+
+  cout << " " << endl;
+  cout << " " << endl;
+  cout << "Fatal Error on this PE: " << msg << endl;
+  cout << " " << endl;
+  Exit();
+
+
+}
+
+
---
+++ codes/esPIC/src/mpiInfo.h
@@ -0,0 +1,287 @@
+
+
+//  ==
+//  ||
+//  ||   C L A S S:    m p i I n f o
+//  ||
+//  ==
+
+class mpiInfo
+{
+ public:
+
+  int myPE;
+  int numPE;
+  int nRealx,  nRealy;
+  int nPEx, nPEy;
+  int iPE , jPE;
+  int iMin, iMax, jMin, jMax ; // The global i-j numbers on this processor
+  int nei_n, nei_s, nei_e, nei_w;
+  int nei_ne, nei_nw, nei_se, nei_sw;
+  
+  int countx, county;
+
+  double *phiL, *phiR;
+  double *phiT, *phiB;
+    
+  double *phiSend_n,  *phiSend_s;
+  double *phiSend_e,  *phiSend_w;
+  double *phiRecv_n,  *phiRecv_s, *phiRecv_e,  *phiRecv_w;
+  
+  MPI_Status  status;
+  int         err;
+  int         tag;
+  MPI_Request request;
+
+  //  -
+  //  |
+  //  |   GridDecomposition: Set up PE numbering system in figure below and
+  //  |                      establish communication arrays.
+  //  |
+  //  |                      nPEx -- number of PEs in the x-direction
+  //  |                      nPEy -- number of PEs in the y-direction
+  //  |                      numPE = total number of PEs
+  //  |
+  //  |                       +-------+-------+ . . .   +-------+
+  //  |                       |       |       |         |       |
+  //  |                       |       |       |         | numPE |
+  //  |                       |       |       |         |       |
+  //  |                       +-------+-------+ . . .   +-------+
+  //  |                       .       .       .         .       .
+  //  |                       .       .       .         .       .
+  //  |                       .       .       .         .       .
+  //  |                       +-------+-------+ . . .   +-------+
+  //  |                       |       |       |         |       |
+  //  |                       | nPEx  | nPEx+1|         |       |
+  //  |                       |       |       |         |       |
+  //  |                       +-------+-------+ . . .   +-------+
+  //  |                       |       |       |         |       |
+  //  |                       |   0   |   1   |         | nPEx-1|
+  //  |                       |       |       |         |       |
+  //  |                       +-------+-------+ . . .   +-------+
+  //  |
+  //  |
+  //  -
+  
+
+  void GridDecomposition(int _nPEx, int _nPEy, int nCellx , int nCelly)
+  {
+
+    nRealx = nCellx + 1;
+    nRealy = nCelly + 1;
+
+    // Store and check incoming processor counts
+    
+    nPEx = _nPEx;
+    nPEy = _nPEy;
+    
+    if (nPEx*nPEy != numPE)
+      {
+    	if ( myPE == 0 ) cout << "Fatal Error:  Number of PEs in x-y directions do not add up to numPE" << endl;
+    	MPI_Barrier(MPI_COMM_WORLD);
+    	MPI_Finalize();
+    	exit(0);
+      }
+    
+    // Get the i-j location of this processor, given its number.  See figure above:
+    
+    jPE = int(myPE/nPEx);
+    iPE = myPE - jPE*nPEx;
+
+    // Set neighbor values
+
+    nei_n = nei_s = nei_e = nei_w = -1;
+
+    if ( iPE > 0      )
+      {
+	nei_w = myPE - 1    ;
+      }
+    if ( jPE > 0      )
+      {
+	nei_s = myPE - nPEx ;  // LabStrip
+      }
+    if ( iPE < nPEx-1 )
+      {
+	nei_e = myPE + 1    ;  // LabStrip
+      }
+    if ( jPE < nPEy-1 )
+      {
+	nei_n = myPE + nPEx ;  // LabStrip
+      }
+
+    nei_nw = nei_sw = nei_ne = nei_se = -1;
+    
+    if ( iPE > 0      && jPE > 0      )  nei_sw = myPE - 1 - nPEx  ;
+    if ( iPE < nPEx-1 && jPE > 0      )  nei_se = myPE + 1 - nPEx  ;
+    if ( iPE > 0      && jPE < nPEy-1 )  nei_nw = myPE - 1 + nPEx  ;
+    if ( iPE > nPEx-1 && jPE < nPEy-1 )  nei_ne = myPE + 1 + nPEx  ;
+
+    // Acquire memory for the communication between adjacent processors:
+    countx = nRealx + 2;
+    county = nRealy + 2;
+    
+    phiL = new double [ county ];	
+    phiR = new double [ county ];	
+    phiT = new double [ countx ];	
+    phiB = new double [ countx ];
+    
+    phiSend_n = new double [ countx ];
+    phiSend_s = new double [ countx ];
+    phiSend_e = new double [ county ];
+    phiSend_w = new double [ county ];
+    phiRecv_n = new double [ countx ];
+    phiRecv_s = new double [ countx ];
+    phiRecv_e = new double [ county ];
+    phiRecv_w = new double [ county ];
+
+    tag = 0;
+  }
+
+  void ExchangeBoundaryInfo(VD &Solution, VD &b)
+  {
+	sLOOP phiSend_n[s] = 0.;
+	sLOOP phiSend_s[s] = 0.;
+	tLOOP phiSend_e[t] = 0.;
+	tLOOP phiSend_w[t] = 0.;
+	
+	// ----------------------------------------------
+	// (1) Parallel communication on PE Boundaries:   ** See fd.h for tLOOP and sLOOP macros **
+	// ----------------------------------------------
+
+	// (1.1) Record values on PE physical boundaries.  These align with the adjacent PE's GHOST NODES, not physical nodes.
+	
+	tLOOP phiL[t] = Solution[ pid(    2 ,    t ) ];      
+	tLOOP phiR[t] = Solution[ pid( nRealx-1 ,    t ) ];
+	sLOOP phiT[s] = Solution[ pid(    s , nRealy-1 ) ];
+	sLOOP phiB[s] = Solution[ pid(    s ,    2 ) ];    
+	
+	// (1.2) Put them into communication arrays
+	
+	sLOOP phiSend_n[s] = phiT[s];
+	sLOOP phiSend_s[s] = phiB[s];
+	tLOOP phiSend_e[t] = phiR[t];
+	tLOOP phiSend_w[t] = phiL[t];
+
+	// (1.3) Send them to neighboring PEs
+
+	if ( nei_n >= 0 )  err = MPI_Isend(phiSend_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request ); 
+	if ( nei_s >= 0 )  err = MPI_Isend(phiSend_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request ); 
+	if ( nei_e >= 0 )  err = MPI_Isend(phiSend_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request ); 
+	if ( nei_w >= 0 )  err = MPI_Isend(phiSend_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request ); 
+
+	// (1.4) Receive values from neighobring PEs' physical boundaries.
+	
+	if ( nei_n >= 0 ) { err = MPI_Irecv(phiRecv_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
+	if ( nei_s >= 0 ) { err = MPI_Irecv(phiRecv_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
+	if ( nei_e >= 0 ) { err = MPI_Irecv(phiRecv_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
+	if ( nei_w >= 0 ) { err = MPI_Irecv(phiRecv_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
+	
+	// (1.5) If new information was received, store it in the candy-coating values
+
+	if ( nei_n >= 0 ) sLOOP { phiT[s] = phiRecv_n[s] ; }  
+	if ( nei_s >= 0 ) sLOOP { phiB[s] = phiRecv_s[s] ; }  
+	if ( nei_e >= 0 ) tLOOP { phiR[t] = phiRecv_e[t] ; }  
+	if ( nei_w >= 0 ) tLOOP { phiL[t] = phiRecv_w[t] ; }  
+	
+	if ( nei_n >= 0 ) sLOOP Solution[ pid( s        , nRealy+1) ] = phiT[s]; 
+	if ( nei_s >= 0 ) sLOOP Solution[ pid( s        , 0       ) ] = phiB[s]; 
+	if ( nei_e >= 0 ) tLOOP Solution[ pid( nRealx+1 , t       ) ] = phiR[t]; 
+	if ( nei_w >= 0 ) tLOOP Solution[ pid( 0        , t       ) ] = phiL[t]; 
+	
+	// (1.2) Apply exchanged information as BCs
+	
+	if ( nei_n >= 0 ) sLOOP b[ pid ( s        , nRealy+1 ) ] = phiT[s] ;  
+	if ( nei_s >= 0 ) sLOOP b[ pid ( s        ,    0     ) ] = phiB[s] ;  
+	if ( nei_e >= 0 ) tLOOP b[ pid ( nRealx+1 ,    t     ) ] = phiR[t] ;  
+	if ( nei_w >= 0 ) tLOOP b[ pid ( 0        ,    t     ) ] = phiL[t] ;  
+  }
+
+  
+  //  ==
+  //  ||
+  //  ||  ParticlesExchange
+  //  ||
+  //  ||  Exchange particles between processors
+  //  ||
+  //  ||
+  //  ==
+
+  void ParticleExchange( VI &ptcl_send_list , VI &ptcl_send_PE , particles &PTCL)
+  {
+
+    // This will be provided on Friday, for Lab 04.  I am waiting to commit this
+    // code for you because there is a student who has not yet submitted the
+    // Lab 03 post-lab report, which involves writing this routine. The focus
+    // this week, in Lab 04, is the routine below.  Please focus on it for
+    // now.  And, again, this routine will be provided for you.
+    
+
+  }
+
+  
+  //  ==
+  //  ||
+  //  || Routine PEsum
+  //  ||
+  //  || This routine receives a field variable named "field"; it has a value for
+  //  || each node in the mesh local to this PE.  However, its value on this PE's
+  //  || boundary nodes is lacking the contributions from the neighboring processor.
+  //  || Here, values in "field" are exchanged between neighboring processors so that
+  //  || each processor can add the contributions from the neighboring processor.
+  //  ||
+  //  ==
+
+  
+  void PEsum( VD &field )
+  {
+
+    //
+    // --------------------------------
+    //     I N S T R U C T I O N S
+    // --------------------------------
+    //
+    // Read the purpose of PEsum in the lines, just above.  It will be used to
+    // sum Qval at nodes on the processes boundaries.
+    //
+    // For debugging purposes, consider disabling the LaplacianOnGrid solver
+    // step as well as the particle exchange and artificially populate Qval[]
+    // yourself in the main routine.  Then call this routine from the main
+    // routine to see if you are exchanging and summing information on the
+    // PE boundaries correctly.
+    //
+    // This routine is the only part you have to write for this lab.  You may use
+    // the same approach used in ExchangeBoundaryInfo, if you want.  For example,
+    // the MPI_Isend() routine will work to send north, south, east, and west.
+    // Likewise, MPI_Irecv() will work to receive.  However, unlike ExchangeBoundaryInfo
+    // there is no need to "reach in one layer" into the other mesh.  Rather, we
+    // are only needing information on the shared boundary nodes.
+    //
+    // Also, remember here that the requirement is to sum values on the shared nodes.
+    // All processes sharing a node should, ultimately, have the same value for
+    // "field".  Your success will be revealed in the Qval 3D plot.  It must be
+    // smooth at PE boundaries.
+    //
+    // If you use the MPI_Isend/Irecv approach, think carefully about the order
+    // in which information will be exchanged.  In particular, think about the
+    // center node, that is shared by, say, four processes.  You want to be careful
+    // not to sum information for that node more times than is necessary -- or less.
+    //
+    // Try to do this lab from scratch.  If you get totally stuck, scaffolding
+    // for this routine can be provided.  But the real learning in this lab will come
+    // from struggling on your own or with your partner regarding the information
+    // flow during the parallel communications...even if using the technology
+    // (MPI_Isend/Irecv) that you already know.
+    //
+    // Once you have this routine working, use it with the real Qval[] and produce
+    // some beautiful particle and voltage plots.
+    //
+    
+
+  }
+
+
+  
+  int pid(int i,int j) { return (i+1) + (j)*(nRealx+2); }  
+
+};
+
