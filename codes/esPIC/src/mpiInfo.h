99d01d2b8651de6fb345f157c4c798f4ddb8ebde
Scott Runnels <SRunnels@SRConsult.com>
2021-10-01 14:44:54 -0600

    Added PEsum so all students can use it for lab 06

--- codes/esPIC/src/mpiInfo.h
+++ codes/esPIC/src/mpiInfo.h
@@ -114,7 +114,7 @@ class mpiInfo
     if ( iPE > 0      && jPE > 0      )  nei_sw = myPE - 1 - nPEx  ;
     if ( iPE < nPEx-1 && jPE > 0      )  nei_se = myPE + 1 - nPEx  ;
     if ( iPE > 0      && jPE < nPEy-1 )  nei_nw = myPE - 1 + nPEx  ;
-    if ( iPE > nPEx-1 && jPE < nPEy-1 )  nei_ne = myPE + 1 + nPEx  ;
+    if ( iPE < nPEx-1 && jPE < nPEy-1 )  nei_ne = myPE + 1 + nPEx  ;
 
     // Acquire memory for the communication between adjacent processors:
     countx = nRealx + 2;
@@ -152,48 +152,48 @@ class mpiInfo
 	
 	tLOOP phiL[t] = Solution[ pid(    2 ,    t ) ];      
 	tLOOP phiR[t] = Solution[ pid( nRealx-1 ,    t ) ];
-	sLOOP phiT[s] = Solution[ pid(    s , nRealy-1 ) ];
-	sLOOP phiB[s] = Solution[ pid(    s ,    2 ) ];    
+	sLOOP phiT[s] = Solution[ pid(    s , nRealy-1 ) ];   // LabReplace("pid(    s , nRealy-1 )" , "      TO-DO in LAB    " )
+	sLOOP phiB[s] = Solution[ pid(    s ,    2 ) ];       // LabReplace("pid(    s ,    2 )" , "      TO-DO in LAB    " )
 	
 	// (1.2) Put them into communication arrays
 	
 	sLOOP phiSend_n[s] = phiT[s];
 	sLOOP phiSend_s[s] = phiB[s];
-	tLOOP phiSend_e[t] = phiR[t];
-	tLOOP phiSend_w[t] = phiL[t];
+	tLOOP phiSend_e[t] = phiR[t];        // LabReplace("phiR[t]","     TO-DO in LAB  " )
+	tLOOP phiSend_w[t] = phiL[t];        // LabReplace("phiL[t]","     TO-DO in LAB  " )
 
 	// (1.3) Send them to neighboring PEs
 
-	if ( nei_n >= 0 )  err = MPI_Isend(phiSend_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request ); 
-	if ( nei_s >= 0 )  err = MPI_Isend(phiSend_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request ); 
-	if ( nei_e >= 0 )  err = MPI_Isend(phiSend_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request ); 
-	if ( nei_w >= 0 )  err = MPI_Isend(phiSend_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request ); 
+	if ( nei_n >= 0 )  err = MPI_Isend(phiSend_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request );    // LabReplace("phiSend_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request " , "  TO-DO in LAB    " )
+	if ( nei_s >= 0 )  err = MPI_Isend(phiSend_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request );    // LabReplace("phiSend_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request " , "  TO-DO in LAB    " )
+	if ( nei_e >= 0 )  err = MPI_Isend(phiSend_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request );    // LabReplace("phiSend_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request " , "  TO-DO in LAB    " )
+	if ( nei_w >= 0 )  err = MPI_Isend(phiSend_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request );    // LabReplace("phiSend_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request " , "  TO-DO in LAB    " )
 
 	// (1.4) Receive values from neighobring PEs' physical boundaries.
 	
-	if ( nei_n >= 0 ) { err = MPI_Irecv(phiRecv_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
-	if ( nei_s >= 0 ) { err = MPI_Irecv(phiRecv_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
-	if ( nei_e >= 0 ) { err = MPI_Irecv(phiRecv_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
-	if ( nei_w >= 0 ) { err = MPI_Irecv(phiRecv_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); } 
+	if ( nei_n >= 0 ) { err = MPI_Irecv(phiRecv_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); }   // LabReplace("phiRecv_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request ","      TO-DO in LAB   ")
+	if ( nei_s >= 0 ) { err = MPI_Irecv(phiRecv_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); }   // LabReplace("phiRecv_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request ","      TO-DO in LAB   ")
+	if ( nei_e >= 0 ) { err = MPI_Irecv(phiRecv_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); }   // LabReplace("phiRecv_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request ","      TO-DO in LAB   ")
+	if ( nei_w >= 0 ) { err = MPI_Irecv(phiRecv_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request );   MPI_Wait(&request,&status); }   // LabReplace("phiRecv_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request ","      TO-DO in LAB   ")
 	
 	// (1.5) If new information was received, store it in the candy-coating values
 
-	if ( nei_n >= 0 ) sLOOP { phiT[s] = phiRecv_n[s] ; }  
-	if ( nei_s >= 0 ) sLOOP { phiB[s] = phiRecv_s[s] ; }  
-	if ( nei_e >= 0 ) tLOOP { phiR[t] = phiRecv_e[t] ; }  
-	if ( nei_w >= 0 ) tLOOP { phiL[t] = phiRecv_w[t] ; }  
+	if ( nei_n >= 0 ) sLOOP { phiT[s] = phiRecv_n[s] ; }   // LabReplace("phiRecv_n[s]" , "  TO-DO in LAB")
+	if ( nei_s >= 0 ) sLOOP { phiB[s] = phiRecv_s[s] ; }   // LabStrip
+	if ( nei_e >= 0 ) tLOOP { phiR[t] = phiRecv_e[t] ; }   // LabStrip
+	if ( nei_w >= 0 ) tLOOP { phiL[t] = phiRecv_w[t] ; }   // LabStrip
 	
-	if ( nei_n >= 0 ) sLOOP Solution[ pid( s        , nRealy+1) ] = phiT[s]; 
-	if ( nei_s >= 0 ) sLOOP Solution[ pid( s        , 0       ) ] = phiB[s]; 
-	if ( nei_e >= 0 ) tLOOP Solution[ pid( nRealx+1 , t       ) ] = phiR[t]; 
-	if ( nei_w >= 0 ) tLOOP Solution[ pid( 0        , t       ) ] = phiL[t]; 
+	if ( nei_n >= 0 ) sLOOP Solution[ pid( s        , nRealy+1) ] = phiT[s];     // LabReplace("phiT[s];","  TO-DO in LAB  ")
+	if ( nei_s >= 0 ) sLOOP Solution[ pid( s        , 0       ) ] = phiB[s];     // LabStrip
+	if ( nei_e >= 0 ) tLOOP Solution[ pid( nRealx+1 , t       ) ] = phiR[t];     // LabStrip
+	if ( nei_w >= 0 ) tLOOP Solution[ pid( 0        , t       ) ] = phiL[t];     // LabStrip
 	
 	// (1.2) Apply exchanged information as BCs
 	
-	if ( nei_n >= 0 ) sLOOP b[ pid ( s        , nRealy+1 ) ] = phiT[s] ;  
-	if ( nei_s >= 0 ) sLOOP b[ pid ( s        ,    0     ) ] = phiB[s] ;  
-	if ( nei_e >= 0 ) tLOOP b[ pid ( nRealx+1 ,    t     ) ] = phiR[t] ;  
-	if ( nei_w >= 0 ) tLOOP b[ pid ( 0        ,    t     ) ] = phiL[t] ;  
+	if ( nei_n >= 0 ) sLOOP b[ pid ( s        , nRealy+1 ) ] = phiT[s] ;      // LabReplace("phiT[s]" , "     TO-DO in LAB  ")
+	if ( nei_s >= 0 ) sLOOP b[ pid ( s        ,    0     ) ] = phiB[s] ;      // LabStrip
+	if ( nei_e >= 0 ) tLOOP b[ pid ( nRealx+1 ,    t     ) ] = phiR[t] ;      // LabStrip
+	if ( nei_w >= 0 ) tLOOP b[ pid ( 0        ,    t     ) ] = phiL[t] ;      // LabStrip
   }
 
   
@@ -207,22 +207,27 @@ class mpiInfo
   //  ==
 
 
-  void ParticleExchange( VI &ptcl_send_list , VI &ptcl_send_PE , particles &PTCL)
+  // ~LabBegin~  ~LabStrip~
+  
+
+    void ParticleExchange( VI &ptcl_send_list , VI &ptcl_send_PE , particles &PTCL)
   {
     
     // (1) Get the max number particles to be send by any particular processor, and make sure all processors  know that number.
 
     int numToSend = ptcl_send_list.size();      int maxToSend;
-    
-    MPI_Allreduce( &numToSend, &maxToSend, 1 , MPI_INT, MPI_MAX, MPI_COMM_WORLD);    
+
+    MPI_Allreduce( &numToSend, &maxToSend, 1 , MPI_INT, MPI_MAX, MPI_COMM_WORLD);    // Lab Strip
 
     // (2) Allocate contributions to the upcoming Gather operation.  Here, "C" for "Contribution" to be Gathered
-                                                                                             
+
+                                                                                              // ~Lab Begin~  ~Lab Replace~ ~maxToSend~  ~   TO-DO ~
     int    *Cptcl_PE;  Cptcl_PE = new int    [maxToSend];  // Particles' destination PEs 
     double *Cptcl_x ;  Cptcl_x  = new double [maxToSend];
     double *Cptcl_y ;  Cptcl_y  = new double [maxToSend];
     double *Cptcl_vx;  Cptcl_vx = new double [maxToSend];
     double *Cptcl_vy;  Cptcl_vy = new double [maxToSend];
+                                                                                              // ~Lab End~
 
     // (3) Populate contributions on all processors for the upcoming Gather operation
 
@@ -233,12 +238,12 @@ class mpiInfo
     
     for ( int i = 0 ; i < ptcl_send_list.size() ; ++i )
       {
-	int id      = ptcl_send_list[ i];
-	Cptcl_PE[i] = ptcl_send_PE  [ i];
-	Cptcl_x [i] = PTCL.x        [id];
-	Cptcl_y [i] = PTCL.y        [id];
-	Cptcl_vx[i] = PTCL.vx       [id];
-	Cptcl_vy[i] = PTCL.vy       [id];
+	int id      = ptcl_send_list[ i];                                                    // ~Lab Replace~ ~[ i]~ ~[ TO-DO ]~
+	Cptcl_PE[i] = ptcl_send_PE  [ i];                                                    // ~Lab Replace~ ~[ i]~ ~[ TO-DO ]~
+	Cptcl_x [i] = PTCL.x        [id];                                                    // ~Lab Replace~ ~[id]~ ~[ TO-DO ]~
+	Cptcl_y [i] = PTCL.y        [id];                                                    // ~Lab Replace~ ~[id]~ ~[ TO-DO ]~
+	Cptcl_vx[i] = PTCL.vx       [id];                                                    // ~Lab Replace~ ~[id]~ ~[ TO-DO ]~
+	Cptcl_vy[i] = PTCL.vy       [id];                                                    // ~Lab Replace~ ~[id]~ ~[ TO-DO ]~
       }
 
     // (5) Allocate and initialize the arrays for upcoming Gather operation to PE0.  The sizeOfGather takes
@@ -251,7 +256,7 @@ class mpiInfo
     //     +------------------+------------------+------------------+------------------+
     //             PE0               PE1                PE2               PE3           
 
-    int sizeOfGather = maxToSend*numPE;                                                    
+    int sizeOfGather = maxToSend*numPE;                                                      // ~Lab Replace~   ~maxToSend*numPE~   ~  TO-DO ~
 
     int    *Gptcl_PE;  Gptcl_PE = new int    [sizeOfGather];                                 
     double *Gptcl_x ;  Gptcl_x  = new double [sizeOfGather];
@@ -267,17 +272,21 @@ class mpiInfo
     
     MPI_Barrier(MPI_COMM_WORLD);
     
+    // ~Lab Begin~   ~Lab ReplacePhrase~   ~(~  ~)~    ~ TO-DO ~
+    
     MPI_Allgather( Cptcl_PE , maxToSend , MPI_INT   , Gptcl_PE , maxToSend , MPI_INT   ,  MPI_COMM_WORLD); 
     MPI_Allgather( Cptcl_x  , maxToSend , MPI_DOUBLE, Gptcl_x  , maxToSend , MPI_DOUBLE,  MPI_COMM_WORLD); 
     MPI_Allgather( Cptcl_y  , maxToSend , MPI_DOUBLE, Gptcl_y  , maxToSend , MPI_DOUBLE,  MPI_COMM_WORLD); 
     MPI_Allgather( Cptcl_vx , maxToSend , MPI_DOUBLE, Gptcl_vx , maxToSend , MPI_DOUBLE,  MPI_COMM_WORLD); 
     MPI_Allgather( Cptcl_vy , maxToSend , MPI_DOUBLE, Gptcl_vy , maxToSend , MPI_DOUBLE,  MPI_COMM_WORLD); 
 
+    // ~Lab End~
+
     MPI_Barrier(MPI_COMM_WORLD);
 
     // (7) Put in vector form so they can be added to PTCL.  These arrays are 1-based.
 
-    int Np = 0;  for ( int i = 0 ; i < sizeOfGather ; ++i ) if ( Gptcl_PE[i] == myPE ) ++Np; 
+    int Np = 0;  for ( int i = 0 ; i < sizeOfGather ; ++i ) if ( Gptcl_PE[i] == myPE ) ++Np;           // ~Lab Replace~   ~sizeOfGather~    ~   TO-DO   ~
     
     VD  std_add_x  ;  std_add_x.resize  ( Np+1 );
     VD  std_add_y  ;  std_add_y.resize  ( Np+1 );
@@ -286,13 +295,13 @@ class mpiInfo
 
     int count = 1;
     for ( int i = 0 ; i < sizeOfGather ; ++i )
-      if ( Gptcl_PE[i] == myPE )            
+      if ( Gptcl_PE[i] == myPE )                                           // ~Lab Replace~  ~myPE~  ~  TO-DO  ~
 	{
-	  std_add_x [count] = Gptcl_x[i];   
-	  std_add_y [count] = Gptcl_y [i];  
-	  std_add_vx[count] = Gptcl_vx[i];  
-	  std_add_vy[count] = Gptcl_vy[i];  
-	  ++count;                          
+	  std_add_x [count] = Gptcl_x[i];                                  // ~Lab Replace~ ~count~  ~  TO-DO ~
+	  std_add_y [count] = Gptcl_y [i];                                 // ~Lab Replace~ ~count~  ~  TO-DO ~
+	  std_add_vx[count] = Gptcl_vx[i];                                 // ~Lab Replace~ ~count~  ~  TO-DO ~
+	  std_add_vy[count] = Gptcl_vy[i];                                 // ~Lab Replace~ ~count~  ~  TO-DO ~
+	  ++count;                                                         // ~Lab Replace~ ~count~  ~  TO-DO ~
 	}
 
     PTCL.add(std_add_x , std_add_y, std_add_vx , std_add_vy );
@@ -303,6 +312,9 @@ class mpiInfo
     if (sizeOfGather > 0 ) { delete[] Gptcl_PE;  delete[] Gptcl_x ;  delete[] Gptcl_y ; delete[] Gptcl_vx ; delete[] Gptcl_vy;  }
 
   }
+
+
+  // ~LabEnd~ 
 
   
   //  ==
@@ -317,51 +329,67 @@ class mpiInfo
   //  ||
   //  ==
 
+  // ~LabBegin~  ~LabStrip~
+  
   
   void PEsum( VD &field )
   {
+    int tag    = 0;
 
-    //
-    // --------------------------------
-    //     I N S T R U C T I O N S
-    // --------------------------------
-    //
-    // Read the purpose of PEsum in the lines, just above.  It will be used to
-    // sum Qval at nodes on the processes boundaries.
-    //
-    // For debugging purposes, consider disabling the LaplacianOnGrid solver
-    // step as well as the particle exchange and artificially populate Qval[]
-    // yourself in the main routine.  Then call this routine from the main
-    // routine to see if you are exchanging and summing information on the
-    // PE boundaries correctly.
-    //
-    // This routine is the only part you have to write for this lab.  You may use
-    // the same approach used in ExchangeBoundaryInfo, if you want.  For example,
-    // the MPI_Isend() routine will work to send north, south, east, and west.
-    // Likewise, MPI_Irecv() will work to receive.  However, unlike ExchangeBoundaryInfo
-    // there is no need to "reach in one layer" into the other mesh.  Rather, we
-    // are only needing information on the shared boundary nodes.
-    //
-    // Also, remember here that the requirement is to sum values on the shared nodes.
-    // All processes sharing a node should, ultimately, have the same value for
-    // "field".  Your success will be revealed in the Qval 3D plot.  It must be
-    // smooth at PE boundaries.
-    //
-    // If you use the MPI_Isend/Irecv approach, think carefully about the order
-    // in which information will be exchanged.  In particular, think about the
-    // center node, that is shared by, say, four processes.  You want to be careful
-    // not to sum information for that node more times than is necessary -- or less.
-    //
-    // Try to do this lab from scratch.  If you get totally stuck, scaffolding
-    // for this routine can be provided.  But the real learning in this lab will come
-    // from struggling on your own or with your partner regarding the information
-    // flow during the parallel communications...even if using the technology
-    // (MPI_Isend/Irecv) that you already know.
-    //
-    // Once you have this routine working, use it with the real Qval[] and produce
-    // some beautiful particle and voltage plots.
-    //
+    // --------------------------------------------------------------------------
+    // (1) Exchange information in the North-South direction
+    // --------------------------------------------------------------------------
     
+    // (1.1) From the full field "field", extract the boundary values into the comm arrays:
+
+    sLOOP phiSend_n[s] = field[ pid(    s , nRealy   ) ];       
+    sLOOP phiSend_s[s] = field[ pid(    s , 1        ) ];
+
+    // (1.2) Send field to the neighboring PEs
+
+    if ( nei_n >= 0 )  err = MPI_Isend(phiSend_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request );
+    if ( nei_s >= 0 )  err = MPI_Isend(phiSend_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request );
+
+    // (1.3) Initialize receiving values at zero.  They will not acquire values unless there is a neighboring PE.
+
+    sLOOP {  phiRecv_n[s] = 0.; phiRecv_s[s] = 0.; }  // top  and bottom
+
+    // (1.4) Receive values from neighboring PEs' physical boundaries.
+	
+    if ( nei_n >= 0 ){ err = MPI_Irecv(phiRecv_n, countx , MPI_DOUBLE , nei_n , tag , MPI_COMM_WORLD , &request); MPI_Wait(&request,&status);}
+    if ( nei_s >= 0 ){ err = MPI_Irecv(phiRecv_s, countx , MPI_DOUBLE , nei_s , tag , MPI_COMM_WORLD , &request); MPI_Wait(&request,&status);}
+
+    // (1.5) Add received values to the "values" array originally provided, along each side.
+	
+    sLOOP { field[ pid(s,1)      ] += phiRecv_s[s]; field[ pid(s,nRealy) ] += phiRecv_n[s];    }  // top and bottom
+
+    
+    // --------------------------------------------------------------------------
+    // (2) Exchange information in the East-West direction (repeat steps, above)
+    // --------------------------------------------------------------------------
+
+    // (2.1)
+
+    tLOOP phiSend_e[t] = field[ pid( nRealx     ,  t ) ];       
+    tLOOP phiSend_w[t] = field[ pid( 1          ,  t ) ];   
+
+    // (2.2)
+
+    if ( nei_e >= 0 ) err = MPI_Isend(phiSend_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request);
+    if ( nei_w >= 0 ) err = MPI_Isend(phiSend_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request);
+
+    // (2.3)
+
+    tLOOP {  phiRecv_e[t] = 0.; phiRecv_w[t] = 0.; }  // left and right sides
+
+    // (2.4)
+
+    if ( nei_e >= 0 ){ err = MPI_Irecv(phiRecv_e, county , MPI_DOUBLE , nei_e , tag , MPI_COMM_WORLD , &request); MPI_Wait(&request,&status);}
+    if ( nei_w >= 0 ){ err = MPI_Irecv(phiRecv_w, county , MPI_DOUBLE , nei_w , tag , MPI_COMM_WORLD , &request); MPI_Wait(&request,&status);}
+
+    // (2.5)
+
+    tLOOP {  field[ pid(1,t)      ] += phiRecv_w[t];	field[ pid(nRealx,t) ] += phiRecv_e[t];      }  // left and right sides
 
   }
 
@@ -371,3 +399,4 @@ class mpiInfo
 
 };
 
+// ~LabEnd~
