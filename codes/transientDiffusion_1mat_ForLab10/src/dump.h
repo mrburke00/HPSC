6e97fb1bcd090713d56c0d5154406c443819f27a
Devin Burke <debu7497@shas0137.rc.int.colorado.edu>
2021-11-05 14:57:17 -0600

    lab10 progress

--- codes/transientDiffusion_1mat_ForLab10/src/dump.h
+++ codes/transientDiffusion_1mat_ForLab10/src/dump.h
@@ -28,7 +28,28 @@
 
 void write_mpiio_dump(mpiInfo &myMPI)
 {
-  
+	MPI_Datatype myRealNodes;
+	
+	int idxStartThisPE  [1] = { 1 };  // Index coordinates of the sub-array inside this PE's array, A
+	int AsizeThisPE     [1] = { nField };  // Size of the A array on this PE    
+	int sub_AsizeThisPE [1] = { nField };  // Size of the A-sub-array on this PE 
+
+	MPI_Type_create_subarray(1, AsizeThisPE, sub_AsizeThisPE, idxStartThisPE, MPI_ORDER_C, MPI_FLOAT, &myRealNodes);
+	
+	MPI_Type_commit(&myRealNodes);
+
+	MPI_Datatype myPartOfGlobal;
+  	
+	int idxStartInGlobal [1] = { myPE * nField };  // Index cordinates of the sub-arrayinside the global array
+  	int AsizeGlobal      [1] = { numPE * nField };  // Size of the global array
+
+	MPI_Type_create_subarray(1, AsizeGlobal, sub_AsizeThisPE, idxStartInGlobal, MPI_ORDER_C, MPI_FLOAT, &myPartOfGlobal);
+
+	MPI_Type_commit(&myPartOfGlobal);
+
+
+
+
 }
 
 
